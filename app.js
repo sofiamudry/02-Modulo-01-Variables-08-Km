566fd3be76bccac575acbd1923fc490f4db5d7c4
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 12:50:58 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Km');
+
+alert('Hola, Bienvenide!');
+
+const km = prompt('Ingrese los kilometros');
+const kmAMillas = (km*0.621371);
+
+alert(`Los kilometros ingresados equivalen a ${kmAMillas} millas`);
